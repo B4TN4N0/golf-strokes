28984539372f7e996978b19587e0d51b38c1abb0
B4TN4N0 <batnano777@gmail.com>
2026-03-13 14:12:29 +0100

    feat(Player): :sparkles: Definir adt player

---
+++ src/main/java/edu/teamrocket/golfstrokes/Player.java
@@ -0,0 +1,22 @@
+package edu.teamrocket.golfstrokes;
+
+class Player extends Object {
+
+    private String initials = "";
+    private Byte handicap;
+    private Short score = 0;
+    private Short points = 0;
+
+    Player(String initials, Byte strokes) {
+        this.initials = initials;
+        this.handicap = strokes;
+    }
+
+    String getInitials() {
+        return this.initials;
+    }
+
+    Byte getHandicap() {
+        return this.handicap;
+    }   
+}
