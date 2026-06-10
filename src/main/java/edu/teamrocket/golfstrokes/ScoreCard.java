c25d940df73e951cb44e4db2425561d41c1a1cc1
B4TN4N0 <batnano777@gmail.com>
2026-06-10 13:42:09 +0200

    feat: :sparkles:

--- src/main/java/edu/teamrocket/golfstrokes/ScoreCard.java
+++ src/main/java/edu/teamrocket/golfstrokes/ScoreCard.java
@@ -1,73 +1,21 @@
 package edu.teamrocket.golfstrokes;
 
 import java.util.Optional;
-import java.util.HashMap;
-import java.util.Map;
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class ScoreCard {
-    // Referencias a los jugadores (son opcionales, por defecto null)
-    private String playerA;
-    private String playerB;
-    private String playerC;
-    private String playerD;
 
-    // Referencia al campo (opcional hasta que se asigne)
+    private Optional<Player> playerA = Optional.empty();
+    private Optional<Player> playerB = Optional.empty();
+    private Optional<Player> playerC = Optional.empty();
+    private Optional<Player> playerD = Optional.empty();
+
+    private final List<Hole> holes = new ArrayList<Hole>();
+
     private Course playersCourse;
 
-    // Colección de hoyos con su par correspondiente (Hoyo -> Par)
-    private final Map<Integer, Integer> holePars = new HashMap<>();
+    public ScoreCard() {};
 
-    // Matriz de puntuaciones: Jugador ('A','B','C','D') -> (Hoyo -> Golpes)
-    private final Map<Player, Map<Integer, Integer>> playerScores = new HashMap<>();
 
-    public ScoreCard() {
-    }
-
-    public void setPlayerA(String playerA){
-        this.playerA = playerA;
-    }
-
-    public void setPlayerB(String playerB){
-        this.playerB = playerB;
-    }
-
-    public void setPlayerC(String playerC){
-        this.playerC = playerC;
-    }
-
-    public void setPlayerD(String playerD){
-        this.playerD = playerD;
-    }
-
-    public void setCourse(Course course) {
-        this.playersCourse = course;
-    }
-
-    public Optional<String> getPlayerA() {
-        return Optional.ofNullable(this.playerA);
-    }
-
-    public Optional<String> getPlayerB() {
-        return Optional.ofNullable(this.playerB);
-    }
-
-    public Optional<String> getPlayerC() {
-        return Optional.ofNullable(this.playerC);
-    }
-
-    public Optional<String> getPlayerD() {
-        return Optional.ofNullable(this.playerD);
-    }
-
-    public Optional<Course> getCourse() {
-        return Optional.ofNullable(this.playersCourse);
-    }
-
-    public void setScore(char player, int holeNumber, int score) {
-
-    }
-
-    public Optional<Integer> getScore(char player, int holeNumber) {
-    }
 }
